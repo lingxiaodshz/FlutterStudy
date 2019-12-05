340e5fc3ee1dea42ef4bb4ab205c80a61be56a5e
lingxiaodshz <lingxiaodshz@163.com>
2019-12-05 15:02:38 +0800

    flutter运行环境修复

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text(
-              'You have pushed the button this many times:',
+//              'You have pushed the button this many times:',
+              'You have clicked the button this many times:',
             ),
             new Text(
               '$_counter',
@@ -107,3 +108,58 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+//import 'package:flutter/material.dart';
+//
+//void main() {
+//  runApp(new SampleApp());
+//}
+
+//class SampleApp extends StatelessWidget {
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return new MaterialApp(
+//      title: 'Sample App',
+//      theme: new ThemeData(
+//        primarySwatch: Colors.blue,
+//      ),
+//      home: new SampleAppPage(),
+//    );
+//  }
+//}
+//
+//class SampleAppPage extends StatefulWidget {
+//  SampleAppPage({Key key}) : super(key: key);
+//
+//  @override
+//  _SampleAppPageState createState() => new _SampleAppPageState();
+//}
+//
+//class _SampleAppPageState extends State<SampleAppPage> {
+//  // Default placeholder text
+//  String textToShow = "I Like Flutter";
+//
+//  void _updateText() {
+//    setState(() {
+//      // update the text
+//      textToShow = "Flutter is Awesome!";
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text("Sample App"),
+//      ),
+//      body: new Center(child: new Text(textToShow)),
+//      floatingActionButton: new FloatingActionButton(
+//        onPressed: _updateText,
+//        tooltip: 'Update Text',
+//        child: new Icon(Icons.update),
+//      ),
+//    );
+//  }
+//}
